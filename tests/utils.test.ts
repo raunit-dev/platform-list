f7bd8530c7ca933dd1741a3e0e0dc77bd6f90091
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2025-11-28 16:22:30 +0800

    Uncomment fetchPlatforms test assertions

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -2,7 +2,7 @@ import { fetchPlatforms } from "../src/utils";
 
 describe("fetchPlatforms", () => {
   it("should return an array of platforms", async () => {
-    // const result = await fetchPlatforms();
-    // expect(Array.isArray(result)).toBeTruthy();
+    const result = await fetchPlatforms();
+    expect(Array.isArray(result)).toBeTruthy();
   });
 });
