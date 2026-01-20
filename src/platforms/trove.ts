716484384caf9851740023fe1b48a6a2e412a970
Geninsus <courmontguillaume@gmail.com>
2026-01-20 08:36:59 +0100

    add trove platform

---
+++ src/platforms/trove.ts
@@ -0,0 +1,16 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+export const platform: PlatformRaw = {
+  id: "trove",
+  name: "Trove",
+  description:
+    "Trove is a leverage trading platform for RWAs, Equities and Collectibles.",
+  links: {
+    website: "https://trovemarkets.com/trade",
+    twitter: "https://x.com/trovemarkets",
+  },
+  tags: ["trading", "dapp", "defi", "rwa", "nft"],
+  platformToken: "Trove4jcQhE1dZGrjBwe8LUEAZizcFDZ35gUHezmjhZ",
+  addedAt: 1768910400000,
+};
+
+export const services: ServiceRaw[] = [];
